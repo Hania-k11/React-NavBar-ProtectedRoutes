327aac1f476374caf0bfaac1a78d5ddebf4bc9f6
Hania <haniaaa0987@example.com>
2024-07-26 11:38:15 +0500

    final

--- src/components/NoPage.jsx
+++ src/components/NoPage.jsx
@@ -6,9 +6,9 @@ const NoPage = () => {
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
       <h1 className="text-4xl font-bold text-gray-800 mb-4">404</h1>
       <p className="text-lg text-gray-600 mb-8">Page Not Found</p>
-      {/* <Link to="/" className="text-blue-500 hover:underline">
-        Go back to Home
-      </Link> */}
+      <Link to="/" className="text-blue-500 hover:underline">
+        Go back 
+      </Link>
     </div>
   );
 };
